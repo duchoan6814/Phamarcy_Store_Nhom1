951e617d3af69cb34c3cc9a8f4fb433c664a2a39
Hoan Truong <hoantruong6814@gmail.com>
2020-11-19 11:27:37 +0700

    hoan thanh render avatar

--- src/GUI/MainSenceControl.java
+++ src/GUI/MainSenceControl.java
@@ -1,19 +1,25 @@
 package GUI;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import DAO.DAONhanVien;
+import entity.HoaDon;
 import entity.NhanVienBanThuoc;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.effect.BlurType;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.Shadow;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
 public class MainSenceControl implements Initializable {
@@ -25,7 +31,8 @@ public class MainSenceControl implements Initializable {
 	public Text lblId;
 	public Text lblSoHoaDon;
 	public StackPane stkOptions;
-	
+	public Circle cirAvatar;
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
@@ -42,14 +49,32 @@ public class MainSenceControl implements Initializable {
 		lblTenNhanVien2.setText(nhanVienBanThuoc.getHoTenDem()+" "+nhanVienBanThuoc.getTen());
 		lblId.setText("ID: "+nhanVienBanThuoc.getId());
 		lblSoHoaDon.setText("Số hóa đơn: "+daoNhanVien.getTotalHoaDonByNhanVien(nhanVienBanThuoc.getId()));
+
+		try {
+			setAvatar();
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+
+
 	}
+
+
+	private void setAvatar() {
+		// TODO Auto-generated method stub
+		//		set avatar
+		InputStream in = new ByteArrayInputStream(nhanVienBanThuoc.getAvatar());
+		Image avatar = new Image(in);
+		cirAvatar.setFill(new ImagePattern(avatar));
+	}
+
 
 	public void setNhanVienBanThuoc(NhanVienBanThuoc nhanVienBanThuoc) {
 		// TODO Auto-generated method stub
 		this.nhanVienBanThuoc = nhanVienBanThuoc;
-		
+
 	}
 
-	
-	
+
+
 }
