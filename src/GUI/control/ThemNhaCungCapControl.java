7754dfe29e1a222a3baaed28e932464fdd7d6439
Hoan Truong <hoantruong6814@gmail.com>
2020-12-14 19:21:09 +0700

    init layout them nha cung cap

---
+++ src/GUI/control/ThemNhaCungCapControl.java
@@ -0,0 +1,30 @@
+package GUI.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class ThemNhaCungCapControl implements Initializable {
+	public TextField txtMaNhaCungCap;
+	public TextField txtTenNhaCungCap;
+	public TextField txtSoDienThoai;
+	public TextField txtFAX;
+	public TextField txtTrangChu;
+	public TextField txtSoNha;
+	public Button btnThem;
+	public Button btnHuy;
+	public ComboBox<String> cmbTinh;
+	public ComboBox<String> cmbQuan;
+	public ComboBox<String> cmbPhuong;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
