b4dc6fe911cbc052a9633085366dca2d2c30c1a0
Hoan Truong <hoantruong6814@gmail.com>
2020-12-16 01:11:36 +0700

    fix layout nha cung cap

--- src/DAO/DAONhaCungCap.java
+++ src/DAO/DAONhaCungCap.java
@@ -157,7 +157,7 @@ public class DAONhaCungCap{
 			while(rs.next()) {
 				NhaCungCap cap = new NhaCungCap();
 				cap.setDiaChi(rs.getString("DiaChi"));
-				cap.setEmail(rs.getString("Fax"));
+				cap.setEmail(rs.getString("Email"));
 				cap.setId(rs.getString("NhaCungCapId"));
 				cap.setSoDienThoai(rs.getString("SoDienThoai"));
 				cap.setTenNhaCungCap(rs.getString("TenNhaCungCap"));
