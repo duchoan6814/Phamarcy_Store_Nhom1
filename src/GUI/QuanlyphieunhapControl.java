1b27f3b0d56a109fc9aa4f1c3776ae1916f368e9
Hoan Truong <hoantruong6814@gmail.com>
2020-12-17 01:44:30 +0700

    xong chuc nang xem chi tiet phieu nhap

--- src/GUI/QuanlyphieunhapControl.java
+++ src/GUI/QuanlyphieunhapControl.java
@@ -1,11 +1,16 @@
 package GUI;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import DAO.DAOPhieuNhap;
+import GUI.control.ChiTietPhieuNhapControl;
 import common.Common;
 import common.PhieuNhapTable;
 import entity.PhieuNhapHang;
@@ -15,15 +20,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javafx.util.Callback;
 
 public class QuanlyphieunhapControl implements Initializable {
 	ObservableList<PhieuNhapTable> data;
@@ -86,8 +95,69 @@ public class QuanlyphieunhapControl implements Initializable {
 		colNgayNhap.setCellValueFactory(new PropertyValueFactory<>("ngayNhap"));
 		colTongTien.setCellValueFactory(new PropertyValueFactory<>("tongTien"));
 		colQuanLy.setCellValueFactory(new PropertyValueFactory<>("quanLy"));
+		
+		createButtonXem();
+		
 		tblPhieuNhap.setItems(data);
 	}
+	private void createButtonXem() {
+		// TODO Auto-generated method stub
+		Callback<TableColumn<common.ChiTietHoaDon, String>, TableCell<common.ChiTietHoaDon, String>> cellFactory
+		= //
+		new Callback<TableColumn<common.ChiTietHoaDon, String>, TableCell<common.ChiTietHoaDon, String>>()
+		{
+			@Override
+			public TableCell call(final TableColumn<common.ChiTietHoaDon, String> param)
+			{
+				final TableCell<common.ChiTietHoaDon, String> cell = new TableCell<common.ChiTietHoaDon, String>()
+				{
+
+					final Button btn = new Button("xem");
+
+					{
+						btn.setStyle("-fx-background-color: green");
+						btn.setOnAction(event -> {
+							TableColumn col = tblPhieuNhap.getColumns().get(1);
+							String data = (String) col.getCellObservableValue(tblPhieuNhap.getItems().get(getIndex())).getValue();
+							PhieuNhapHang hang = daoPhieuNhap.getPhieuNhapHangById(data);
+							try {
+								FileInputStream stream = new FileInputStream(new File("src/GUI/ChiTietPhieuNhap.fxml"));
+								FXMLLoader loader = new FXMLLoader();
+								ChiTietPhieuNhapControl control = new ChiTietPhieuNhapControl(hang);
+								loader.setController(control);
+								Stage stage = loader.load(stream);
+								stage.show();
+							} catch (FileNotFoundException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (IOException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						});
+					}
+
+					@Override
+					public void updateItem(String item, boolean empty)
+					{
+						super.updateItem(item, empty);
+						if (empty) {
+							setGraphic(null);
+							setText(null);
+						}
+						else {
+							setGraphic(btn);
+							setText(null);
+						}
+					}
+				};
+				return cell;
+			}
+		};
+
+		colXem.setCellFactory(cellFactory);
+	}
+
 	private void initbtnTim() {
 		btnTim.setOnAction(new EventHandler<ActionEvent>() {
 			
